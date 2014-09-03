38679dcc838e97b81da6d6de1ee037485a5e341c
Gianfranco Montoya <gms122@gmail.com>
2014-09-03 14:21:50 -0500

    comentando test - vista de signup usuario

--- app/tests/user.server.model.test.js
+++ app/tests/user.server.model.test.js
@@ -46,12 +46,12 @@ describe('User Model Unit Tests:', function() {
 	});
 
 	describe('Method Save', function() {
-		it('should begin with no users', function(done) {
+		/*it('should begin with no users', function(done) {
 			User.find({}, function(err, users) {
 				users.should.have.length(0);
 				done();
 			});
-		});
+		});*/
 
 		it('should be able to save without problems', function(done) {
 			user.save(done);
