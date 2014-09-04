95a82614d108108e15a50e3c5cf7c30815bf967c
Ricardo Díaz <ricardotk002@gmail.com>
2014-09-04 16:43:25 -0500

    posts schema uploaded

--- app/models/post.server.model.js
+++ app/models/post.server.model.js
@@ -12,6 +12,23 @@ var mongoose = require('mongoose'),
 var PostSchema = new Schema({
 	// Post model fields   
 	// ...
+	userId: 	{
+		type: Schema.Types.ObjectId,
+		required: true
+	},
+	detail: 	{
+		type: String,
+		trim: true,
+		required: 'Please fill in a textfield',
+	},
+	likes: 		{
+	    type	: [{type: Schema.Types.ObjectId, required: true}],
+	    default	: []
+	},
+	comments: 		{
+	    type	: [{type: Schema.Types.ObjectId, required: true}],
+	    default	: []
+	},
 });
 
 mongoose.model('Post', PostSchema);
--- app/tests/post.server.model.test.js
+++ app/tests/post.server.model.test.js
@@ -4,9 +4,9 @@
  * Module dependencies.
  */
 var should = require('should'),
-	mongoose = require('mongoose'),
-	User = mongoose.model('User'),
-	Post = mongoose.model('Post');
+    mongoose = require('mongoose'),
+    User = mongoose.model('User'),
+    Post = mongoose.model('Post');
 
 /**
  * Globals
@@ -17,39 +17,75 @@ var user, post;
  * Unit tests
  */
 describe('Post Model Unit Tests:', function() {
-	beforeEach(function(done) {
-		user = new User({
-			firstName: 'Full',
-			lastName: 'Name',
-			displayName: 'Full Name',
-			email: 'test@test.com',
-			username: 'username',
-			password: 'password'
-		});
+    beforeEach(function(done) {
+        user = new User({
+            companyId: 'test@test.com',
+            tottusId: 'test@test.com',
+            password: 'password',
+            username: 'username1232',
+            provider: 'local',
+            email: 'test@test.com',
+            isRegistered: 'false',
+            assets: {
+                    profilePicURL       : 'http://sadasdsa.com'
+            },
+            contact: {
+                    phoneNumHome       : '95823453'
+            },
+            demographic: {
+                    maritalStatus       : 'single',
+                    children       : '0'
+            },
+            address: {
+                    city       : 'Lima',
+                    address          : 'Av. Flora Trista 433'
+            },
+            personal: {
+                firstName       : 'Carmen',
+                lastName        : 'Perez',
+                DNI     : '54784931',
+                dateOfBirth    : '12/4/1992',
+                gender : 'M',
+                educationLevel    : 'B. S.',
+                carnetCONADIS    : '421323435',
+                interests : 'Music, Dota2'
+            },
+            organizational: {
+                    currentJobPosition       : 'Reponedor',
+                    area        : 'Pasillo',
+                    branch    : 'Atencion al Publico',
+                    branchId    : 'E12',
+                    jobType    : 'Atencion al Publico',
+                    jobPositionHistory: {
+                        name: 'Panadero',
+                        startDate: '12/1/2014',
+                        endDate: '10/5/2014',
+                        location: 'Atocongo',
+                    },
+                    phoneNumTottus : '013458395'
+            }
+        });
 
-		user.save(function() { 
-			post = new Post({
-				// Add model fields
-				// ...
-			});
+        user.save();
 
-			done();
-		});
-	});
+        post = new Post({
+            detail:     "test",
+            userId:     user._id
+        });
 
-	describe('Method Save', function() {
-		it('should be able to save without problems', function(done) {
-			return post.save(function(err) {
-				should.not.exist(err);
-				done();
-			});
-		});
-	});
+        done();
+    });
 
-	afterEach(function(done) { 
-		Post.remove().exec();
-		User.remove().exec();
+    describe('Method Save', function() {
+        it('should be able to save without problems', function(done) {
+            post.save(done);
+        });
+    });
 
-		done();
-	});
+    afterEach(function(done) { 
+        Post.remove().exec();
+        User.remove().exec();
+
+        done();
+    });
 });
