9c01b1ec5537a1eee351fa8824c00d5f9a0b617e
Gianfranco Montoya <gms122@gmail.com>
2014-09-12 15:08:38 -0500

    comments - posts

--- app/routes/comment.server.routes.js
+++ app/routes/comment.server.routes.js
@@ -5,23 +5,24 @@ module.exports = function(app) {
         comments = require('../../app/controllers/comment');
 
     // --------------------backend--------------------
-    // route               verb                method
-    // /comments              POST                create post     => post.create()
-    // /comments/:id          GET                 show post       => post.show()
-    // /comments/:id          PUT                 update posts    => post.update()
-    // /comments/:id          DELETE              delete posts    => post.delete()
-
-
+    // route                                verb                method
+    // /comments                            GET                 comments list             => post.create()
+    // /comments/:commentId                 GET                 show comment              => post.show()
+    //
+    // /posts/:postId/comments              GET                 list comment per post     => comment.create() =)
+    // /posts/:postId/comments/:commentId   GET                 show comment              => comment.show()   
+    // /posts/:postId/comments/:commentId   PUT                 update comment            => comment.update()
+    // /posts/:postId/comments/:commentId   DELETE              delete comment            => comment.delete()
 
     // --------------------frontend--------------------
     // state               verb                method
     // /comments/new          GET                 create on angular
 
    
-    app.route('/comments').get(comments.index);
+    app.route('/posts/:postId/comments').get(comments.index);
     app.route('/comments').post(users.requiresLogin, comments.create);
-    app.route('/comments/:commentId').get(comments.show);
-    app.route('/comments/:commentId').put(users.requiresLogin, comments.hasAuthorization, comments.update);
-    app.route('/comments/:commentId').delete(users.requiresLogin, comments.hasAuthorization, comments.delete);
+    app.route('/posts/:postId/comments/:commentId').get(comments.show);
+    app.route('/posts/:postId/comments/:commentId').put(users.requiresLogin, comments.hasAuthorization, comments.update);
+    app.route('/posts/:postId/comments/:commentId').delete(users.requiresLogin, comments.hasAuthorization, comments.delete);
 
 };	
