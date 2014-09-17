70ec9895b0acf6b5c8623f09c1b6ec3614d73f03
Gus <yo@soygus.com>
2014-09-17 16:47:23 -0500

    Added modals

--- public/modules/users/controllers/modals.client.controller.js
+++ public/modules/users/controllers/modals.client.controller.js
@@ -2,12 +2,12 @@
 
 angular.module('users').controller('ModalController', ['$scope', '$modal', '$log', function ($scope, $modal, $log) {
       $modal.open({
-                templateUrl: 'modules/users/views/modals/change-password-modal.client.view.html',
-                controller: function($scope, $modalInstance) {
-                    $scope.ok = function() {
-                        $modalInstance.close();
-                    };
-                }
-            });
+        templateUrl: 'modules/users/views/modals/change-password-modal.client.view.html',
+        controller: function($scope, $modalInstance) {
+          $scope.ok = function() {
+            $modalInstance.close();
+          };
+        }
+      });
     }
 ]);
